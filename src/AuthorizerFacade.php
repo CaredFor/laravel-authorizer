be1faf1e977a3d1c8577a6f9011ca583b0c1d0fd
Ben Wilkins <bentwilkins@gmail.com>
2018-06-05 17:45:25 -0500

    Adding docblock for IDE hinting

--- src/AuthorizerFacade.php
+++ src/AuthorizerFacade.php
@@ -6,6 +6,15 @@ namespace Benwilkins\Authorizer;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class AuthorizerFacade
+ * @package Benwilkins\Authorizer
+ *
+ * @method static permissions()
+ * @method static roles()
+ * @method static getClass(string $modelName)
+ * @method static flushCache(string $type = null)
+ */
 class AuthorizerFacade extends Facade
 {
     protected static function getFacadeAccessor()
